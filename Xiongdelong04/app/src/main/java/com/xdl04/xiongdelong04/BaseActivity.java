b8f62bc64531bd58e4a670fcb6f8641288d7048b
xdl <you@example.com>
2020-06-29 20:42:43 +0800

    fcv

---
+++ Xiongdelong04/app/src/main/java/com/xdl04/xiongdelong04/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.xdl04.xiongdelong04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
